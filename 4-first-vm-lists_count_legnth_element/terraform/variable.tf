8a4ed2ed95d0b13a23502a6fb9e4f57fa7a0ce80
purplefeetguy <purplefeetguy@hotmail.com>
2020-03-08 08:02:44 -0500

    first VM lists, count and element

---
+++ 4-first-vm-lists_count_legnth_element/terraform/variable.tf
@@ -0,0 +1,11 @@
+variable "prefix" {
+  default = "pfl-tfvmex"
+}
+
+variable "location" {
+  default = "North Central US"
+}
+
+variable "name_count" {
+  default = ["server1", "server2", "server3"]
+}
