8ad05702f2ca13da2129340735c4b7b92f7b1803
purplefeetguy <purplefeetguy@hotmail.com>
2020-03-09 07:43:05 -0500

    core-infra external managed-disk; updated NIC to use count

---
+++ core-infrastructure/4-core-infra-external-disks/terraform/main.tf
@@ -0,0 +1,60 @@
+provider "azurerm" {
+  version         = "~> 1.44"
+  alias           = "PFL"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-resources"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_network_interface" "main" {
+  count               = 1
+  name                = "${var.prefix}-nic-${count.index + 1}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+output "vnet_name" {
+  value = "${azurerm_virtual_network.main.*.name}"
+}
+
+output "subnet_name" {
+  value = "${azurerm_subnet.internal.*.name}"
+}
+
+output "virtual_machine_name" {
+  value = "${azurerm_virtual_machine.main.*.name}"
+}
+
+output "virtual_machine_interface" {
+  value = "${azurerm_network_interface.main.*.name}"
+}
+
+output "virtul_machine_location" {
+  value = "${azurerm_virtual_machine.main.*.location}"
+}
--- core-infrastructure/4-core-infra-external-disks/terraform/virtual_machine.tf
+++ core-infrastructure/4-core-infra-external-disks/terraform/virtual_machine.tf
@@ -1,18 +1,9 @@
-
-resource "azurerm_managed_disk" "disk" {
-  name                 = "${var.prefix}-managed_disk"
-  location             = var.location
-  resource_group_name  = azurerm_resource_group.main.name
-  storage_account_type = "Standard_LRS"
-  disk_size_gb         = 10
-}
-
 resource "azurerm_virtual_machine" "main" {
   count                 = 1
   name                  = "${var.prefix}-${count.index + 1}"
   location              = var.location
   resource_group_name   = azurerm_resource_group.main.name
-  network_interface_ids = [azurerm_network_interface.main.id]
+  network_interface_ids = [azurerm_network_interface.main[0].id]
   vm_size               = "Standard_DS1_v2"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
@@ -36,27 +27,11 @@ resource "azurerm_virtual_machine" "main" {
   }
 
   storage_data_disk {
-    name              = "${var.prefix}-data_disk-01"
+    name              = "${var.prefix}-data_disk"
     lun               = 1
     managed_disk_type = "Standard_LRS"
     create_option     = "Empty"
     disk_size_gb      = 10
-  }
-
-  storage_data_disk {
-    name              = "${var.prefix}-data_disk-02"
-    lun               = 2
-    managed_disk_type = "Standard_LRS"
-    create_option     = "Empty"
-    disk_size_gb      = 10
-  }
-
-  storage_data_disk {
-    name            = "${azurerm_managed_disk.disk.name}"
-    lun             = 3
-    managed_disk_id = azurerm_resource_group.main.id
-    create_option   = "Attach"
-    disk_size_gb    = azurerm_managed_disk.disk.disk_size_gb
   }
 
   os_profile {
